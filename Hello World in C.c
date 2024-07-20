058f2a591ebc078d97d9e424256bc8ddf13d1458
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-20 20:54:59 +0530

    Update Hello World in C.c

--- Hello World in C.c
+++ Hello World in C.c
@@ -1,1 +1,12 @@
 #  https://www.hackerrank.com/challenges/hello-world-c/problem?isFullScreen=true
+
+#include<stdio.h>
+#include<string.h>
+int main() {
+    char s[100];
+    scanf("%[^\n]%*c",s);
+    printf("Hello, World!\n");
+    printf("%s\n",s);
+    
+    return 0;
+}
