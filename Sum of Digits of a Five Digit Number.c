61ddde81a78ff286d678c3886e52ff9b7aee7569
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-20 20:59:50 +0530

    Update Sum of Digits of a Five Digit Number.c

--- Sum of Digits of a Five Digit Number.c
+++ Sum of Digits of a Five Digit Number.c
@@ -1,1 +1,19 @@
 #  https://www.hackerrank.com/challenges/sum-of-digits-of-a-five-digit-number/problem?isFullScreen=true
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n;
+    scanf("%d", &n);
+    int sum = 0;
+    for(int i=0;i<=5;i++) {
+        sum += n%10;
+        n/=10;
+    }
+    printf("%d\n",sum);
+    return 0;
+}
