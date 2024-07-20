e64378685ae7ff6b0dc0da8fc42a501b1463e515
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-20 20:57:04 +0530

    Update Pointers in C.c

--- Pointers in C.c
+++ Pointers in C.c
@@ -1,1 +1,21 @@
 #  https://www.hackerrank.com/challenges/pointer-in-c/problem?isFullScreen=true
+
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    int sum = *a + *b;
+    int abs_diff = abs(*a-*b);
+    *a = sum;
+    *b = abs_diff;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
